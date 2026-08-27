b192f9bebb64fe237a18b0365d6d3da4a9919298
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-08-27 17:43:30 -0300

    feat: testando diferentes layouts

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,99 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(MinhaWidget());
+ // runApp(MinhaApp());
 }
 
+/*
+class MinhaApp extends StatelessWidget{
+  Widget build(BuildContext bc){
+    return MaterialApp(
+        title: "Ola Mundo!",
+        home: Scaffold(
+            appBar: AppBar(title: Text("OlaApp")),
+            body:
+            Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.star, color: Colors.amber),
+                  Icon(Icons.star, color: Colors.amber),
+                  Icon(Icons.star, color: Colors.amber),
+                ])
+
+            /*Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Icon(Icons.star, color: Colors.amber),
+                  Icon(Icons.star, color: Colors.amber),
+                  Icon(Icons.star, color: Colors.amber),
+                ])*/
+        )
+    );
+  }
+}
+*/
+
+/*
+class MinhaApp extends StatelessWidget {
+  Widget build(BuildContext bc){
+    return MaterialApp(
+      title: 'Minha App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Minha App'),
+          actions: [
+            IconButton(
+              onPressed: () => print('pressionado!'),
+              icon: Icon(Icons.three_k),
+              )
+          ],
+          ),
+        body: Padding(
+          padding: EdgeInsets.all(30),
+          child:
+            ListView(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.black54,
+                   ),
+                   child:
+                      Column(
+                        children: [
+                          Text('Dica do dia'),
+                          Text('Utilize ListView para criar listas')
+                        ]
+                      ),
+                ),
+                SizedBox(
+                  height: 20
+                ),
+                Text('Novo Componente!'),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: 
+                    InputDecoration(
+                      hintText: "Faça a pesquisa aqui",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                      icon: Icon(Icons.search)
+                      ),
+                ),
+              ]),
+            ),
+          )
+    );
+  }
+}
+*/
+
+/* 
 class MinhaWidget extends StatelessWidget{
   Widget build(BuildContext bc){
     return MaterialApp(
@@ -40,3 +130,4 @@ class MinhaWidget extends StatelessWidget{
     );
   }
 }
+*/
